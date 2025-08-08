a8e59f2b1a2c13c71a3be8b8a5e029e64355d199
Boi Nguyen <boi@devpanel.com>
2025-08-08 15:31:15 +0700

    update script

--- .devpanel/custom_package_installer.sh
+++ .devpanel/custom_package_installer.sh
@@ -39,7 +39,7 @@ echo '> Install shopware package';
 echo '> bin/console system:install --basic-setup --create-database --force';
 cd $APP_ROOT
 sudo bin/console system:install --basic-setup --create-database --force
-#sudo chown -R www:www public/ vendor/ var/
+sudo chown -R www:www public/ vendor/ var/
 
 # Install profiler and other dev tools, eg Faker for demo data generation
 composer require --dev shopware/dev-tools
