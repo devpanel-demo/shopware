3bae5b4f4df8d023c80cb947a9cac859b1d43d1e
Boi Nguyen <boi@devpanel.com>
2025-09-15 14:17:21 +0700

    check db

--- .devpanel/create_quickstart.sh
+++ .devpanel/create_quickstart.sh
@@ -171,7 +171,7 @@ du -h $DUMPS_DIR/db.sql
 
 echo -e "> Compress database"
 tar -czf "$DUMPS_DIR/db.sql.tgz" -C $DUMPS_DIR db.sql
-rm -rf $DUMPS_DIR/db.sql $DUMPS_DIR/tmp_file.sql
+# rm -rf $DUMPS_DIR/db.sql $DUMPS_DIR/tmp_file.sql
 
 # Step 2 - Compress static files
 mkdir -p $APP_ROOT/.devpanel/dumps
